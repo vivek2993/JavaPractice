6d401b3e710d79b641a6827f53185e3cf27dfc83
Vivek <vivek.kumar@velocis.co.in>
2024-06-22 21:11:52 +0530

    commited on 22nd

--- Child.java
+++ Child.java
@@ -3,7 +3,7 @@ public class Child extends Parent{
 
 	   public Child() {
 	        super(); // Call to superclass constructor
-	        System.out.println("Child class constructor is called");
+	        System.out.println("Child class constructor is called...");
 	    }
 	   
 	   public static void main(String[] args) {
